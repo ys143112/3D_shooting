bbe64358732f70a84672e3ac918f2c8d7b1c7443
정연수 <haruhic192@gmail.com>
2022-06-15 22:51:49 +0900

    06.15 코루틴 긑. 닷트윈 운석에 넣기(과제)

--- Assets/Scripts/GameOverUI.cs
+++ Assets/Scripts/GameOverUI.cs
@@ -9,6 +9,8 @@ public class GameOverUI : MonoBehaviour
     public Text voidTime = null;
     public Text pathDistance = null;
     public Button closeButton = null;
+    public GameObject InGame = null;
+    public GameObject Main = null;
 
     private string voidTimeStr = null;
     private string pathDistanceStr = null;
@@ -56,8 +58,23 @@ public class GameOverUI : MonoBehaviour
         }
     }
 
-    void closeTab()
+    public void closeTab()
     {
+        StartCoroutine(FadeIn());
+    }
 
+    IEnumerator FadeIn()
+    {
+        
+        Color color = closeButton.image.color;
+        for(float i=1f;i>0f;i -= 0.1f)
+        {
+            color.a = i;
+            closeButton.image.color = color;
+
+            yield return new WaitForSeconds(0.01f);
+        }
+        InGame.SetActive(false);
+        Main.SetActive(true);
     }
 }
